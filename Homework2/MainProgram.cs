892977e58ff275f663f0b2c4daefbb5668937116
김승우 <@KIMSEUNGWOO>
2023-08-21 16:49:22 +0900

    초기 커밋.

---
+++ Homework2/MainProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            TextFileReader txtImporter = new TextFileReader();
+            Console.WriteLine("파일 주소를 입력해주세요");
+            string filePath = Console.ReadLine();
+            Console.WriteLine("\n");
+            string[] textFile = txtImporter.txtImport(filePath);
+        }
+    }
+}
---
+++ Homework2/TextFileReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class TextFileReader
+    {
+        public string[] txtImport(string filePath)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                string[] textValue = System.IO.File.ReadAllLines(filePath);
+                return textValue;
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+                return null;
+            }
+        }
+    }
+}
